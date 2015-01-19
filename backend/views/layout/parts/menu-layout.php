1d999eaf7ef2f0670f16f4e387e5962a031fa163
Piotr Pażuchowski <piotr.pazuchowski@gmail.com>
2015-01-19 21:42:58 +0100

    dodanie ikonek

--- backend/views/layout/parts/menu-layout.php
+++ backend/views/layout/parts/menu-layout.php
@@ -2,28 +2,28 @@
     <div class="container">
 
         <div class="navbar-header">
-            <a class="navbar-brand" href="<?php echo BASE_APP_FOLDER; ?>/">Simple Blog</a>
+            <a class="navbar-brand" href="<?php echo BASE_APP_FOLDER; ?>/"><i class="glyphicon glyphicon-home"></i> Blog</a>
         </div>
 
         <ul class="nav navbar-nav">
             <li>
-                <a href="<?php echo BASE_APP_FOLDER; ?>/">Lista postów</a>
+                <a href="<?php echo BASE_APP_FOLDER; ?>/"><i class="glyphicon glyphicon glyphicon-th-list"></i> Lista postów</a>
             </li>
 
             <?php if (!UserModel::isLogged()): ?>
 
                 <li>
-                    <a href="<?php echo BASE_APP_FOLDER; ?>/user/login">Zaloguj</a>
+                    <a href="<?php echo BASE_APP_FOLDER; ?>/user/login"><i class="glyphicon glyphicon-user"></i> Zaloguj</a>
                 </li>
 
             <?php else: ?>
 
                 <li>
-                    <a href="<?php echo BASE_APP_FOLDER; ?>/post/add">Dodaj post</a>
+                    <a href="<?php echo BASE_APP_FOLDER; ?>/post/add"><i class="glyphicon glyphicon glyphicon-plus"></i> Dodaj post</a>
                 </li>
 
                 <li>
-                    <a href="<?php echo BASE_APP_FOLDER; ?>/user/logout">Wyloguj</a>
+                    <a href="<?php echo BASE_APP_FOLDER; ?>/user/logout"><i class="glyphicon glyphicon glyphicon-off"></i> Wyloguj</a>
                 </li>
 
             <?php endif; ?>
@@ -34,7 +34,7 @@
             <div class="form-group">
                 <input id="inputSearchQuery" name="inputSearchQuery" value="<?php echo $inputSearchQuery; ?>" type="text" class="form-control" placeholder="Wpisz szukaną frazę">
             </div>
-            <button type="submit" class="btn btn-default">Szukaj</button>
+            <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i> Szukaj</button>
         </form>
 
     </div>
